88f801f6d07bf7e0608e68fe6034269f1d1b01d9
MaximeBolduc <34164070+Maymayz44@users.noreply.github.com>
2022-11-02 08:53:28 -0400

    Créer la classe Rectangle

---
+++ ExerciceDocumentation/ExerciceDocumentation/Rectangle.cs
@@ -0,0 +1,24 @@
+﻿namespace ExerciceDocumentation
+{
+    public class Rectangle
+    {
+        private int longeur;
+        private int hauteur;
+
+
+        public event EventHandler<int> AireCalculee;
+
+        public Rectangle(int longeur, int hauteur)
+        {
+            this.longeur = longeur;
+            this.hauteur = hauteur;
+        }
+
+        public int CalculerAire()
+        {
+            int aire = longeur * hauteur;
+            AireCalculee?.Invoke(this, aire);
+            return aire;
+        }
+    }
+}
