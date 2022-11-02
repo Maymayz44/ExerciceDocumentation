327e9fd5a3c5f51a0b1f40b409fee807b157be44
MaximeBolduc <34164070+Maymayz44@users.noreply.github.com>
2022-11-02 08:54:54 -0400

    Ajouter les balises de documentation

--- ExerciceDocumentation/ExerciceDocumentation/Rectangle.cs
+++ ExerciceDocumentation/ExerciceDocumentation/Rectangle.cs
@@ -8,12 +8,21 @@
 
         public event EventHandler<int> AireCalculee;
 
+        /// <summary>
+        /// Crée l'objet et définit ses paramètres
+        /// </summary>
+        /// <param name="longeur">La longeur du rectangle</param>
+        /// <param name="hauteur">La hauteur du rectangle</param>
         public Rectangle(int longeur, int hauteur)
         {
             this.longeur = longeur;
             this.hauteur = hauteur;
         }
 
+        /// <summary>
+        /// Calcule l'aire du rectangle
+        /// </summary>
+        /// <returns>L'aire du rectangle</returns>
         public int CalculerAire()
         {
             int aire = longeur * hauteur;
